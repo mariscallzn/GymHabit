ff80e080d29bf6443af5e75e7b71f5cb7cb2bf33
Andres Mariscal <mariscal.lzn@gmail.com>
2020-12-09 19:39:49 -0600

    WIP

--- app/src/main/java/com/andymariscal/gymhabit/MainApplication.kt
+++ app/src/main/java/com/andymariscal/gymhabit/MainApplication.kt
@@ -1,13 +1,17 @@
 package com.andymariscal.gymhabit
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
+
+        //Timber setup
+        if(BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
     }
